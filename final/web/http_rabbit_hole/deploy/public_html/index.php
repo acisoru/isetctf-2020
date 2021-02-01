6878b842fcf683e3891c5f8a164cea2038028248
Sergei Antonov <antonov.sergey.s@yandex.ru>
2021-02-01 12:16:20 +0300

    Add files for http_rabbit_hole

---
+++ final/web/http_rabbit_hole/deploy/public_html/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>WoW</title>
+    <link rel="stylesheet" href="static/style.css">
+</head>
+<body>
+    <main>
+        <section class="center-block">
+            <form>
+                <h1>Rabbit hole?</h1>
+                <label>
+                    <input type="text" class="input" name="login" placeholder="Login">
+                </label>
+                <label>
+                    <input type="password" class="input" name="password" placeholder="Password">
+                </label>
+                <button>where is the [secret]?</button>
+            </form>
+        </section>
+    </main>
+</body>
+</html>
---
+++ final/web/http_rabbit_hole/deploy/public_html/secret/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Try!</title>
+    <link rel="stylesheet" href="static/style.css">
+</head>
+<body>
+    <main>
+        <section class="center-block">
+            <form>
+                <h1>Rabbit hole?</h1>
+                <h5>where is the [flag{wh47_15_4_H77P_v3r8_74mP3R1n9}]?</h5>
+            </form>
+        </section>
+    </main>
+</body>
+</html>
